370ef80020790b2ea3fd614c4413a0cdf208585c
Adea <tabakuadea3@gmail.com>
2024-12-22 21:46:15 +0100

    gradle build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,9 +29,19 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+    packagingOptions {
+
+        resources {
+            excludes += "/META-INF/{NOTICE.md,NOTICE.txt,LICENSE.md,LICENSE.txt}"
+        }
+    }
 }
 
 dependencies {
+
+    implementation ("com.sun.mail:android-mail:1.6.7")
+    implementation ("com.sun.mail:android-activation:1.6.7")
+    implementation ("org.mindrot:jbcrypt:0.4")
 
     implementation(libs.appcompat)
     implementation(libs.material)
